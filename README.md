476721ba8662d7891d6945116e4b00d035721277
Nathan Anderson <nanderson4@RRC.ca>
2024-01-23 08:26:42 -0600

    Initial commit. Documented our project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Module 2 Demonstration
+
+## Description
+Introduction to Python: Documentation, Variables, Printing, Lists, Sets, Dictionaries, Tuples
+
+## Author
+Nathan Anderson
---
+++ module_02.py
@@ -0,0 +1,13 @@
+"""
+Description: Module 2 Demonstration
+Author: Nathan Anderson
+Date: 2024-01-23
+Usage: To demonstrate content from module 2
+"""
+
+"""
+This is a
+mult-line comment.
+"""
+
+# This is a single comment
