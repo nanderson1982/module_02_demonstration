a3844489cf3b100bd2f8e2ba5f6e0f91175c6b04
Nathan Anderson <nanderson4@RRC.ca>
2024-01-23 09:48:55 -0600

    Experimented with Lists.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Module 2 Demonstration
 
 ## Description
-Introduction to Python: Documentation, Variables, Printing, Lists, Sets, Dictionaries, Tuples
+Introduction to Python: Documentation, Variables, Printing, Lists, Sets, Dictionaries, Tuples, Booleans
 
 ## Author
 Nathan Anderson
--- module_02.py
+++ module_02.py
@@ -90,3 +90,83 @@ print(age)
 
 result = ((10 + 5) *2) / 3 - 1
 print(result)
+
+# Collect Types
+# A list of integers. 
+daily_step_count = [10343, 9385, 7029, 10931, 5921]
+print(daily_step_count)
+
+# A list of mixed data values. 
+employee_data = ["A123", 55024.23, 595, True] 
+print(employee_data)
+
+# A list of lists. 
+list_of_lists = [["A", "B", "C"], [1, "X", True], [False, 12, 5.5]]
+print(list_of_lists)
+
+print(daily_step_count[2])
+
+daily_step_count[4] = 100 
+print(daily_step_count) 
+
+# Appends the value 8694 to the end of the list. 
+daily_step_count.append(8694) 
+print(daily_step_count) 
+
+# Inserts the value 4473 before the current element 3.  
+daily_step_count.insert(3, 4473) 
+print(daily_step_count) 
+
+# Removes the first occurrence of 7029 from the list. 
+daily_step_count.remove(7029) 
+print(daily_step_count) 
+
+# Removes the last item from the list.
+# captures it into the popped variable. 
+popped = daily_step_count.pop() 
+print(daily_step_count) 
+print(popped)
+# output: [10343, 9385, 10931, 5921]
+
+# Sets index_value to the index of the first 4473. 
+index_value = daily_step_count.index(4473) 
+print(daily_step_count)
+print(index_value) 
+
+#index_value = daily_step_count.index(9999) 
+#output: an exception occurs because the value 9999 does not exist in the list.
+
+
+daily_step_count = [10343, 9385, 7029, 10931, 5921, 5921] 
+
+# Sets count to the number of occurrences of 5921. 
+count = daily_step_count.count(5921) 
+print(count)
+
+daily_step_count = [10343, 9385, 7029, 10931, 5921] 
+
+# Sorts in ascending order.
+daily_step_count.sort() 
+print(daily_step_count) 
+
+daily_step_count.sort(reverse=True)
+print(daily_step_count)
+
+# Reverses the order of the items in the list.
+daily_step_count.reverse() 
+print(daily_step_count) 
+
+# Slicing
+red_river = ['R', 'R', 'C', ' ', 'P', 'o', 'l', 'y', 't', 'e', 'c', 'h', 'n', 'i', 'c'] 
+
+print(red_river[2: 12: 2])
+
+print(red_river[: 10: 1]) 
+
+print(red_river[5: : 3]) 
+
+print(red_river[::5]) 
+
+print(red_river[-1: -5: -1]) 
+
+# Tuples
