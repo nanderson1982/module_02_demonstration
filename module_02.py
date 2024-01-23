928c5ffbbf7adc8c483801a3ec8c37c1895fc49d
Nathan Anderson <nanderson4@RRC.ca>
2024-01-23 08:47:39 -0600

    Experimented with built-in functions.

--- module_02.py
+++ module_02.py
@@ -11,3 +11,26 @@ mult-line comment.
 """
 
 # This is a single comment
+
+absolute_value = abs(-12)
+print('absolute value:', absolute_value)
+
+# embedded functions
+print('absolute value:', abs(-12))
+
+print("I am", 25, "years old")
+print("I am 25 years old")
+
+print("apples", "oranges", "bananas", sep= ", ")
+
+name = "John"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
+
+value = 3.14159423423434654645
+number = 123
+
+print(f"The value is {value:.4f}.")
+print(f"The number is {number:05}.")
+
+print("Hello\nWorld.")
