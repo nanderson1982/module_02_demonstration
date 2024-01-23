53ac77e9ae7c1a76a51b8d4a7039c584462a9e08
Nathan Anderson <nanderson4@RRC.ca>
2024-01-23 09:15:32 -0600

    Experimented with declaring variables of different data types and type conversions.

--- module_02.py
+++ module_02.py
@@ -34,3 +34,29 @@ print(f"The value is {value:.4f}.")
 print(f"The number is {number:05}.")
 
 print("Hello\nWorld.")
+
+print(type(name))
+print(type(value))
+print(type(number))
+
+print("Value as an int:", int(value))
+print("Number as float:", float(number))
+
+FEDERAL_TAX_RATE = 0.05
+
+# Strings
+original_string = "hello, world!"
+original_string = original_string.capitalize()
+print(original_string)
+
+width = 20
+char = '-'
+
+centered_string = original_string.center(width, char)
+print(centered_string)
+
+uppercase = original_string.upper()
+print(uppercase)
+
+replaced_string = original_string.replace("Hello", "hi")
+print(replaced_string)
